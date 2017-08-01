b94ee4093541a85d399158047070eb868ad3a950
VonUniGE <VonUniGE@github.com>
2017-08-01 09:08:25 +0000

    Set url and mirror correctly
    Follow the recommendation of brew audit --strict
    Please use "https://ftp.gnu.org/gnu/cpio/cpio-2.12.tar.bz2" instead of
    https://ftpmirror.gnu.org/cpio/cpio-2.12.tar.bz2

--- Formula/cpio.rb
+++ Formula/cpio.rb
@@ -1,8 +1,8 @@
 class Cpio < Formula
   desc "Copies files into or out of a cpio or tar archive"
   homepage "https://www.gnu.org/software/cpio/"
-  url "https://ftpmirror.gnu.org/cpio/cpio-2.12.tar.bz2"
-  mirror "https://ftp.gnu.org/gnu/cpio/cpio-2.12.tar.bz2"
+  url "https://ftp.gnu.org/gnu/cpio/cpio-2.12.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/cpio/cpio-2.12.tar.bz2"
   sha256 "70998c5816ace8407c8b101c9ba1ffd3ebbecba1f5031046893307580ec1296e"
   # tag "linuxbrew"
 
